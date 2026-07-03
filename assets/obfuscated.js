0721326f08ae8e20111aa849ba5d9e4b08057f8e
kurt zamora <114384916+sp3crtre@users.noreply.github.com>
2026-07-03 13:12:10 +0800

    Add files via upload

---
+++ assets/obfuscated.js
@@ -0,0 +1,10 @@
+// Obfuscated JavaScript - Deobfuscate to find the secret
+var _0x1a2b = ['js_is_fun', 'Hello from obfuscated JS'];
+var _0x3c4d = function(_0x5e6f) {
+    return _0x5e6f.split('').reverse().join('');
+};
+var _0x7a8b = _0x3c4d('nuf_si_sj');
+console.log(_0x3c4d('!SJ detacifubO olleH'));
+
+// The secret value is in _0x1a2b[0], but reversed
+// Hint: reverse('nuf_si_sj') = ?
